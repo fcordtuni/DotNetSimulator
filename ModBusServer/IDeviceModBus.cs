9920eab71060ec153d3a1b28c4bbaaf660c4964c
LissyLady <64424696+LissyLady@users.noreply.github.com>
2023-10-27 15:25:01 +0200

    added device interface

---
+++ ModBusServer/IDeviceModBus.cs
@@ -0,0 +1,31 @@
+﻿public interface IDeviceModbus
+{
+    int Seriennummer { get; }
+    int HerstellerID { get; }
+    int ProduktID { get; }
+    int ProduktTyp { get; }
+    int Versionsnummer { get; }
+    int MaxLeistung { get; }
+    int MaxSpannung { get; }
+    int MaxStrom { get; }
+    int Wirkungsgrad { get; }
+    int Frequenzausgang { get; }
+    int Leistungsausgang { get; }
+    int Spannungsausgang { get; }
+    int Stromausgang { get; }
+    int Frequenz { get; }
+    int Betriebsmodus { get; }
+    int Betriebsstatus { get; }
+    int Temperatur { get; }
+    int Fehlercodes { get; }
+    int Betriebsmoduseinstellung { get; }
+    int MaximaleLeistungseinstellung { get; }
+    int Spannungseinstellung { get; }
+    int Frequenzeinstellung { get; }
+    int Schutzfunktionseinstellungen { get; }
+    int Einschaltverzögerung { get; }
+    int Kommunikationseinstellungen { get; }
+    int UpdateIntervalle { get; }
+    int Energiesparmodus { get; }
+    int StartStopFunktion { get; }
+}
