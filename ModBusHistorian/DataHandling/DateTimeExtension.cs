f6875961ded51b22dd00025cfe101a3d2e1c8775
LissyLady <64424696+LissyLady@users.noreply.github.com>
2024-01-12 18:47:19 +0100

    added data handling

---
+++ ModBusHistorian/DataHandling/DateTimeExtension.cs
@@ -0,0 +1,15 @@
+﻿//Author: Elisabeth Gisser
+
+namespace ModBusHistorian.DataHandling;
+
+internal static class DateTimeExtensions
+{
+    public static long ToUnixEpochInMilliSecondsTime(this DateTime dateTime)
+    {
+        if (dateTime == DateTime.MinValue)
+            return 0;
+        DateTime dateTime1 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        TimeSpan timeSpan = dateTime - dateTime1;
+        return timeSpan.TotalMilliseconds >= 0.0 ? (long)timeSpan.TotalMilliseconds : throw new ArgumentOutOfRangeException("Unix epoc starts January 1st, 1970");
+    }
+}
